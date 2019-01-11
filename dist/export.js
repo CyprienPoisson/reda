c2e396f80a60234bf640e3893adff40068d27c5a
Cyprien Poisson <c.poisson@3h15.com>
2019-01-11 15:53:20 +0100

    Remove dist from git.

--- dist/export.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var child_process_1 = require("child_process");
-var users = child_process_1.execSync("synouser --enum all")
-    .match(/^(.*?)$/gm)
-    .slice(1);
-console.log(users);
